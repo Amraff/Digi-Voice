4c41fb7faf90a1b854e240ad0a4833e603e2d311
Amraff <raphewkarim@gmail.com>
2025-09-18 22:24:18 +0000

    Update

--- terraform/apigateway.tf
+++ terraform/apigateway.tf
@@ -352,4 +352,9 @@ resource "aws_api_gateway_stage" "prod" {
   deployment_id = aws_api_gateway_deployment.deploy_v2.id
   rest_api_id   = aws_api_gateway_rest_api.polly_api.id
   stage_name    = "prod"
+  
+  variables = {
+    deployed_at = timestamp()
+    auth_mode = "none"
+  }
 }
